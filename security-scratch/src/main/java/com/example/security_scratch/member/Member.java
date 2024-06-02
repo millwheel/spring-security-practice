8d8c4fa65ebab03c22b6a4dfd183450635fe283c
season <sionwer4@naver.com>
2024-06-02 22:39:05 +0900

    feat: Add annotation of jpa to member

--- security-scratch/src/main/java/com/example/security_scratch/member/Member.java
+++ security-scratch/src/main/java/com/example/security_scratch/member/Member.java
@@ -1,16 +1,22 @@
 package com.example.security_scratch.member;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
 
 @Entity
 public class Member {
 
+    @Id
     private Long id;
 
+    @Column
     private String username;
 
+    @Column
     private String name;
 
+    @Column
     private String password;
 
 }
