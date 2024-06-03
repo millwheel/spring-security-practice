db8df67121b0369b3dc81d239a1358a0af44de15
박시준 <sjpark@logblack.com>
2024-06-03 19:43:45 +0900

    refactor: Add custom user by spring bean

--- spring-security/src/main/java/com/example/spring_security/SecurityConfig.java
+++ spring-security/src/main/java/com/example/spring_security/SecurityConfig.java
@@ -5,6 +5,9 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 @EnableWebSecurity
@@ -16,5 +19,14 @@ public class SecurityConfig{
         http.authorizeHttpRequests(auth -> auth.anyRequest().authenticated())
                 .formLogin(Customizer.withDefaults());
         return http.build();
+    }
+
+    @Bean
+    public InMemoryUserDetailsManager inMemoryUserDetailsManager(){
+        UserDetails user = User.withUsername("user")
+                .password("{noop}1111")
+                .authorities("ROLE_USER")
+                .build();
+        return new InMemoryUserDetailsManager(user);
     }
 }
