e9e6d51ee9d055edb092906631e7ead33889f163
박시준 <sjpark@logblack.com>
2024-06-03 14:40:28 +0900

    feat: Add password hashing

--- security-scratch/src/main/java/com/example/security_scratch/member/Member.java
+++ security-scratch/src/main/java/com/example/security_scratch/member/Member.java
@@ -1,5 +1,6 @@
 package com.example.security_scratch.member;
 
+import com.example.security_scratch.util.PasswordHashing;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
@@ -14,12 +15,12 @@ public class Member {
     private String username;
 
     @Column
-    private String password;
+    private String hashedPassword;
 
     public static Member createMember(String username, String password){
         Member member = new Member();
         member.setUsername(username);
-
+        member.setPassword(PasswordHashing.hashPassword(password));
         return member;
     }
 
@@ -27,12 +28,8 @@ public class Member {
         this.username = username;
     }
 
-    private void setPassword(String password) {
-        this.password = password;
-    }
-
-    private void hashPassword(String password){
-
+    private void setPassword(String hashedPassword){
+        this.hashedPassword = hashedPassword;
     }
 
 }
