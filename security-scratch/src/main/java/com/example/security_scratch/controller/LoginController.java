e2d15a5dd6cfd1888618b607b54ad54f171305ff
박시준 <sjpark@logblack.com>
2024-06-03 17:41:48 +0900

    feat: Add session manager to Login

--- security-scratch/src/main/java/com/example/security_scratch/controller/LoginController.java
+++ security-scratch/src/main/java/com/example/security_scratch/controller/LoginController.java
@@ -2,6 +2,7 @@ package com.example.security_scratch.controller;
 
 import com.example.security_scratch.dto.LoginDto;
 import com.example.security_scratch.service.LoginService;
+import com.example.security_scratch.util.SessionManager;
 import jakarta.servlet.http.Cookie;
 import jakarta.servlet.http.HttpServletResponse;
 import jakarta.validation.Valid;
@@ -15,18 +16,13 @@ import org.springframework.web.bind.annotation.*;
 public class LoginController {
 
     private final LoginService loginService;
+    private final SessionManager sessionManager;
 
     @PostMapping
     @ResponseStatus(HttpStatus.OK)
     public void login(@RequestBody @Valid LoginDto loginDto, HttpServletResponse response){
         Long memberId = loginService.login(loginDto.getUsername(), loginDto.getPassword());
-        // Create a session cookie
-        Cookie cookie = new Cookie("memberId", String.valueOf(memberId));
-        cookie.setHttpOnly(true);
-        cookie.setPath("/");
-        // Set cookie max age to 1 day
-        // cookie.setMaxAge(60 * 60 * 24);
-        response.addCookie(cookie);
+        sessionManager.createSession(memberId, response);
     }
 
 }
