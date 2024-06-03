9ca930e19fb536f8987484c9ae536a222fc3b1cb
박시준 <sjpark@logblack.com>
2024-06-03 14:37:40 +0900

    feat: Create password Hashing class

---
+++ security-scratch/src/main/java/com/example/security_scratch/util/PasswordHashing.java
@@ -0,0 +1,26 @@
+package com.example.security_scratch.util;
+
+import lombok.extern.slf4j.Slf4j;
+
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
+@Slf4j
+public class PasswordHashing {
+
+    public static String hashPassword(String password) {
+        MessageDigest md;
+        try {
+            md = MessageDigest.getInstance("SHA-256");
+        } catch (NoSuchAlgorithmException e) {
+            log.error("No algorithm: SHA-256");
+            throw new RuntimeException(e);
+        }
+        byte[] hashedPassword = md.digest(password.getBytes());
+        StringBuilder sb = new StringBuilder();
+        for (byte b : hashedPassword) {
+            sb.append(String.format("%02x", b));
+        }
+        return sb.toString();
+    }
+}
