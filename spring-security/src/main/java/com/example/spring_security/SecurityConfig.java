57847d4bc8a04343908517e2a8218a012102e065
박시준 <sjpark@logblack.com>
2024-06-05 22:54:17 +0900

    feat: remember me 기능 사용

--- spring-security/src/main/java/com/example/spring_security/SecurityConfig.java
+++ spring-security/src/main/java/com/example/spring_security/SecurityConfig.java
@@ -3,10 +3,12 @@ package com.example.spring_security;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
@@ -17,18 +19,23 @@ public class SecurityConfig{
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        http.authorizeHttpRequests(auth -> auth.anyRequest().authenticated())
-                .formLogin(form -> form
-                        .loginProcessingUrl("/login/process")
-                        .successForwardUrl("/home")
-                        .failureUrl("/login")
-                        .permitAll()
+        http
+                .authorizeHttpRequests(auth -> auth.anyRequest().authenticated())
+                .formLogin(Customizer.withDefaults())
+                .rememberMe(rememberMe -> rememberMe
+                        .alwaysRemember(true)
+                        .tokenValiditySeconds(3600)
+                                .userDetailsService(userDetailsService())
+                                .rememberMeParameter("remember")
+                                .rememberMeCookieName("remember")
+                                .key("security")
                 );
+
         return http.build();
     }
 
     @Bean
-    public InMemoryUserDetailsManager inMemoryUserDetailsManager(){
+    public UserDetailsService userDetailsService(){
         UserDetails user = User.withUsername("user")
                 .password("{noop}1234")
                 .roles("USER")
