d942791d0f847a3051a83f41f7884a18faaa3180
박시준 <sjpark@logblack.com>
2024-06-06 12:07:15 +0900

    feat: security context holder 사용

--- spring-security/src/main/java/com/example/spring_security/controller/LoginController.java
+++ spring-security/src/main/java/com/example/spring_security/controller/LoginController.java
@@ -1,7 +1,13 @@
 package com.example.spring_security.controller;
 
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.context.SecurityContext;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 public class LoginController {
@@ -19,6 +25,14 @@ public class LoginController {
     @GetMapping("/logout/success")
     public String afterLogout(){
         return "logout success";
+    }
+
+    @GetMapping("/roles")
+    public String index(){
+        SecurityContext context = SecurityContextHolder.getContextHolderStrategy().getContext();
+        Authentication authentication = context.getAuthentication();
+        List<String> authoritiesStrings = authentication.getAuthorities().stream().map(GrantedAuthority::getAuthority).toList();
+        return String.format("including authority: %s", authoritiesStrings.get(0));
     }
 
 }
