da422ce2bb3c309c16fa880d9eb758932f9b3d1e
millwheel <sionwer4@naver.com>
2024-08-16 17:52:27 +0900

    fix: form authentication provider support method fixed

--- spring-security-master/src/main/java/io/security/springsecuritymaster/security/provider/FormAuthenticationProvider.java
+++ spring-security-master/src/main/java/io/security/springsecuritymaster/security/provider/FormAuthenticationProvider.java
@@ -7,7 +7,6 @@ import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.AuthenticationException;
-import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Component;
@@ -35,6 +34,6 @@ public class FormAuthenticationProvider implements AuthenticationProvider {
 
     @Override
     public boolean supports(Class<?> authentication) {
-        return false;
+        return authentication.isAssignableFrom(UsernamePasswordAuthenticationToken.class);
     }
 }
